4ee8e1234ff121273baf91885f51fe7d0beb5923
Bernhard Übelacker <bernhardu@mailbox.org>
2021-08-30 13:41:57 +0200

    RR-DEBUG: ntdll: Enable recording with rr of cygwin executables.

--- dlls/ntdll/unix/env.c
+++ dlls/ntdll/unix/env.c
@@ -347,7 +347,8 @@ static BOOL is_special_env_var( const char *var )
             STARTS_WITH( var, "TMP=" ) ||
             STARTS_WITH( var, "QT_" ) ||
             STARTS_WITH( var, "VK_" ) ||
-            STARTS_WITH( var, "XDG_SESSION_TYPE=" ));
+            STARTS_WITH( var, "XDG_SESSION_TYPE=" ) ||
+            STARTS_WITH( var, "LD_" ));   /* don't forward rr's LD_PRELOAD to cygwin */
 }
 
 /* check if an environment variable changes dynamically in every new process */
